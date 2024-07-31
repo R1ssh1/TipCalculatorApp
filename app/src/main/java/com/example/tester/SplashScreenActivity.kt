b3bbce1baa287189f78fa4d1ee44e22749869498
R1ssh1 <rishi.sk.j@gmail.com>
2024-07-31 22:22:56 +0530

    updated to v1.2, succesfully implemented planned changes. Added splash screen succesfuly

---
+++ app/src/main/java/com/example/tester/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tester
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
